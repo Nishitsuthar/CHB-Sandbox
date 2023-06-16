8d3b1470b7dd5ea7c0bd0572df0238ce5b5ec16a
Nishit <nishitsuthar123@gmail.com>
2023-06-16 16:56:07 +0530

    CHB-58 Done

--- force-app/main/default/aura/Price_Book_Entry_RecordPage/Price_Book_Entry_RecordPageController.js
+++ force-app/main/default/aura/Price_Book_Entry_RecordPage/Price_Book_Entry_RecordPageController.js
@@ -1,7 +1,7 @@
 ({
     doInit : function(component, event, helper) {
        helper.doInithelper(component, event, helper)
-    }, 
+    },
 
     editRecord : function(component, event, helper){
         console.log('Edit Record');
@@ -12,22 +12,22 @@
 
         helper.doInithelper(component, event, helper)
         component.set("v.viewMode", false);
-    }, 
+    },
     leaveEditForm : function(component, event, helper){
         console.log('Leave Edit Form');
         $A.get('e.force:refreshView').fire();
         component.set("v.viewMode", true);
-    }, 
+    },
 
     saveRecord : function(component, event, helper){
         console.log('Save Record');
         var temp = component.get("v.pbEntry");
-        debugger;
         console.log('check your input s ',{temp});
-	
+
         var action = component.get("c.updateRecord");
         action.setParams({
-            pbEntry : component.get("v.pbEntry")
+            pbEntry : component.get("v.pbEntry"),
+            oldMarkup : oldValueMarkup
         });
         action.setCallback(this, function (response) {
             component.set("v.viewMode", true);
@@ -49,5 +49,5 @@
         });
         $A.enqueueAction(action);
 
-    }, 
+    },
 })
